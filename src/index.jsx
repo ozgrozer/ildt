d82842520761593141ccbba30600c6417cff4e5e
ozgrozer <ozgr@live.com>
2018-10-21 16:37:42 +0300

    Fix the image URLs.

--- src/index.jsx
+++ src/index.jsx
@@ -13,7 +13,7 @@ class App extends React.Component {
           {
             objectsList.map((objectName, key) => {
               return (
-                <img key={key} src={`/objects/${objectName}.png`} alt={objectName} />
+                <img key={key} src={`objects/${objectName}.png`} alt={objectName} />
               )
             })
           }
@@ -21,15 +21,15 @@ class App extends React.Component {
 
         <div className='scene'>
           <Rnd default={{ x: 350, y: 155, width: 300 }} minWidth={10} minHeight={10} lockAspectRatio>
-            <img src='/objects/table2.png' alt='' draggable='false' />
+            <img src='objects/table2.png' alt='' draggable='false' />
           </Rnd>
 
           <Rnd default={{ x: 430, y: 120, width: 90 }} minWidth={10} minHeight={10} lockAspectRatio>
-            <img src='/objects/imac.png' alt='' draggable='false' />
+            <img src='objects/imac.png' alt='' draggable='false' />
           </Rnd>
 
           <Rnd default={{ x: 490, y: 230, width: 90 }} minWidth={10} minHeight={10} lockAspectRatio>
-            <img src='/objects/keyboard.png' alt='' draggable='false' />
+            <img src='objects/keyboard.png' alt='' draggable='false' />
           </Rnd>
         </div>
       </div>
